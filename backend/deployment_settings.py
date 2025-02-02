0925361efa65db17b450e8818cba5188340e51cd
Engineed <teamengineed@gmail.com>
2025-02-02 20:31:11 +0800

    eto nanaman tayo

--- backend/deployment_settings.py
+++ backend/deployment_settings.py
@@ -5,8 +5,8 @@ from .settings import BASE_DIR
 
 
 
-ALLOWED_HOSTS = [os.environ.get['RENDER_EXTERNAL_HOSTNAME']]
-CSRF_TRUSTED_ORIGINS = ['https://'+os.environ.get['RENDER_EXTERNAL_HOSTNAME']]
+ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
+CSRF_TRUSTED_ORIGINS = ['https://'+os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
 
 DEBUG = False
 SECRET_KEY = os.environ.get['SECRET_KEY']
